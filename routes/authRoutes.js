71f3fcf8aef801eb112f32bbdffd7bb0227c56e7
anti-k-camino <kratyukan@gmail.com>
2018-02-16 22:06:58 +0200

    logout handled

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,6 +13,11 @@ module.exports = app => {
 
   app.get('/auth/github/callback', passport.authenticate('github'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user || 'No longer signed in'); // to prove no longer signed in
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
