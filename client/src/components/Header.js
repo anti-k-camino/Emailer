63f87da8abdde3d2c2eaf6c9c37483c6a6ba85a3
anti-k-camino <kratyukan@gmail.com>
2018-03-06 11:03:42 +0200

    basic authentication flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <div>
+            <li><a href="/auth/google">Login With Google</a></li>
+          </div>
+        );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render () {
     return (      
       <nav>
-        <div className="nav-wrapper">
+        <div className="nav-wrapper grey darken-1">
           <a className="left brand-logo">Emailer</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a>Login With Google</a></li>
-            <li><a>Login With Github</a></li>
+          <ul className="right">
+            { this.renderContent() }
           </ul>
         </div>
       </nav>      
@@ -16,4 +32,8 @@ class Header extends Component {
   }
 };
 
-export default Header;
+function mapStateToProps ({ auth }) {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,25 +1,32 @@
 const passport = require('passport');
 
 module.exports = app => {
+  // Google
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
-
+  app.get('/auth/google/callback',
+   passport.authenticate('google'),
+   (req, res) => { res.redirect('/surveys'); }
+  );
+  // GitHub
   app.get('/auth/github', passport.authenticate('github', {
     scope: ['profile', 'email']
   }));
 
-  app.get('/auth/github/callback', passport.authenticate('github'));
+  app.get('/auth/github/callback',
+   passport.authenticate('github'),
+   (req, res) => { res.redirect('/surveys'); }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user || 'No longer signed in'); // to prove no longer signed in
+    // res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
-    // res.send(req.session);
     res.send(req.user);
   });
 };
